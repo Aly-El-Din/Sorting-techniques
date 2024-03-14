4798d7bdf0f4efcd3bbaf67d8440e02dc797b76d
Amir-Ragaie <amir.ragaie.soliman@gmail.com>
2024-03-14 14:32:55 +0200

    Setting up type 2 class

--- EfficientSort.java
+++ EfficientSort.java
@@ -3,24 +3,88 @@ import java.util.Arrays;
 
 public class EfficientSort {
     void printIntermediate(ArrayList<Integer> iteration) {
-        System.out.println(Arrays.toString(iteration.toArray()));
+        System.out.print(Arrays.toString(iteration.toArray()));
     }
-
-    public ArrayList<Integer> MergeSort(ArrayList<Integer> array, boolean wantSteps){
-        if(wantSteps) {
-            printIntermediate(array);
+    boolean sortedTest(ArrayList<Integer> arr){
+        for(int i = 1 ;i < arr.size(); i++){
+            if(arr.get(i-1) > arr.get(i)){
+                return false;
+            }
         }
-        return array;
+        return true;
     }
-    public static void main(String[] args) {
-        System.out.println(2);
-        ArrayList<Integer> lis = new ArrayList<>();
-        lis.add(1);
-        lis.add(5);
-        lis.add(2);
-        EfficientSort e = new EfficientSort();
-        System.out.print(e.MergeSort(lis,false));
+
+    ArrayList<Integer> MergeSort(ArrayList<Integer> array, boolean wantSteps){
+        int size = array.size();
+        if(wantSteps){
+            System.out.print("Array: ");
+            printIntermediate(array);
+            System.out.println();
+        }
+        if(size < 2 ){
+            return array;
+        }
+        int mid = size / 2 ;
+        ArrayList<Integer> left = new ArrayList<>(mid);
+        ArrayList<Integer> right = new ArrayList<>(size - mid);
+        //left initialization
+        for (int i = 0; i < mid; i++) {
+            left.add(array.get(i));
+        }
+        //right initialization
+        for (int i = mid; i < size; i++) {
+            right.add(array.get(i));
+        }
+        if(wantSteps){
+            System.out.print("Left: ");
+            printIntermediate(left);
+            System.out.print(" Right: ");
+            printIntermediate(right);
+            System.out.println();
+        }
+        left = MergeSort(left,wantSteps);
+        right = MergeSort(right,wantSteps);
+
+        return merge(left,right,wantSteps);
 
 
+
     }
+    ArrayList<Integer> merge(ArrayList<Integer> left , ArrayList<Integer> right, boolean wantSteps){
+        // Initial pointers of first and second subarrays
+        int leftPointer = 0, rightPointer = 0;
+        ArrayList<Integer> mergeIn = new ArrayList<>(left.size()+right.size());
+
+
+        // Initial index of merged subarray array
+        while (leftPointer < left.size() && rightPointer < right.size()) {
+            if (left.get(leftPointer) <= right.get(rightPointer)) {
+                mergeIn.add(left.get(leftPointer)) ;
+                leftPointer ++;
+            } else {
+                mergeIn.add(right.get(rightPointer)) ;
+                rightPointer ++;
+            }
+        }
+
+        /* Copy remaining elements of left if any */
+        while (leftPointer < left.size()) {
+            mergeIn.add(left.get(leftPointer)) ;
+            leftPointer ++;
+        }
+
+        /* Copy remaining elements of right if any */
+        while (rightPointer < right.size()) {
+            mergeIn.add(right.get(rightPointer)) ;
+            rightPointer ++;
+        }
+            if(wantSteps){
+                System.out.print("Merged: ");
+                printIntermediate(mergeIn);
+                System.out.println();
+            }
+        return mergeIn;
+    }
+
+
 }
---
+++ Test.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Random;
+public class Test {
+    public static ArrayList<Integer> generateRandomIntegers(int size, int min, int max) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            int randomNumber = random.nextInt(max - min + 1) + min;
+            arrayList.add(randomNumber);
+        }
+
+        return arrayList;
+    }
+    public static void main(String[] args) {
+        System.out.println(2);
+        ArrayList<Integer> lis = generateRandomIntegers(7,0,100);
+
+        EfficientSort e = new EfficientSort();
+        ArrayList<Integer> sorted = e.MergeSort(lis,true);
+        System.out.println(sorted);
+        System.out.print(e.sortedTest(sorted));
+
+
+    }
+}
